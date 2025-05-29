12dbb72f08bde696aaed85081068cf65e4432709
Tonni Tielens <tonni.tielens@gmail.com>
2025-05-29 14:15:59 +0200

    Add EnumerableExtensions

---
+++ Tonnet.Tests/EnumerableExtensionsTest.cs
@@ -0,0 +1,34 @@
+﻿namespace Tonnet.Tests
+{
+    [TestClass]
+    public class EnumerableExtensionsTest
+    {
+        [TestMethod]
+        public void EmptyIfNull_ReturnsEmptyEnumerable_WhenSourceIsNull()
+        {
+            // Arrange
+            IEnumerable<int>? source = null;
+
+            // Act
+            var result = source.EmptyIfNull();
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.IsFalse(result.Any());
+        }
+
+        [TestMethod]
+        public void EmptyIfNull_ReturnsSource_WhenSourceIsNotNull()
+        {
+            // Arrange
+            List<int> source = [1, 2, 3];
+
+            // Act
+            var result = source.EmptyIfNull();
+
+            // Assert
+            Assert.IsNotNull(result);
+            Assert.AreSame(source, result);
+        }
+    }
+}
---
+++ Tonnet/EnumerableExtensions.cs
@@ -0,0 +1,20 @@
+﻿namespace Tonnet
+{
+    public static class EnumerableExtensions
+    {
+        /// <summary>
+        /// This function helps with using possibly null enumerable collections
+        /// in Linq statements by returning an empty enumerable if the source is
+        /// null.
+        /// </summary>
+        /// <typeparam name="T">The type of elements in the collection.</typeparam>
+        /// <param name="source">The enumerable collection to check.</param>
+        /// <returns>The source enumerable if it is not null, or an empty enumerable if it is.</returns>
+        public static IEnumerable<T> EmptyIfNull<T>(this IEnumerable<T>? source)
+        {
+            if (source == null) return [];
+
+            return source;
+        }
+    }
+}
